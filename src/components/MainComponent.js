78dc7dcefc4d7315ee2a90001780b338d13e5a89
Stephen Ubong <stevelov1982@gmail.com>
2023-01-22 13:50:39 +0100

    Assignment 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -44,6 +45,9 @@ class Main extends Component {
       );
     }
 
+
+    
+
     return (
       <div>          
         <Header />
@@ -51,6 +55,7 @@ class Main extends Component {
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>                 
